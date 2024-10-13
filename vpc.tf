8cf9610f9bb852af0188a5c9b58be30441c40477
vinod <100143638+vinod-reddy-1998@users.noreply.github.com>
2024-10-13 21:24:18 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,16 +1,33 @@
+provider "aws" {
+  region = "us-east-1"  # Change to your desired region
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+data "aws_subnet" "default_subnets" {
+  count = length(data.aws_subnet_ids.default.ids)
+  id    = data.aws_subnet_ids.default.ids[count.index]
+}
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~> 4.0"
 
-  name = local.name
-  cidr = local.vpc_cidr
+  name              = "default-vpc"  # Give a name for identification
+  cidr              = data.aws_vpc.default.cidr_block
 
-  azs             = local.azs
-  private_subnets = local.private_subnets
-  public_subnets  = local.public_subnets
-  intra_subnets   = local.intra_subnets
+  azs              = data.aws_vpc.default.azs  # Use availability zones from the default VPC
+  private_subnets  = [for subnet in data.aws_subnet.default_subnets : subnet.id]  # Use existing subnets
+  public_subnets   = []  # You can define public subnets if needed
+  intra_subnets    = []  # Define intra subnets if required
 
-  enable_nat_gateway = true
+  enable_nat_gateway = false  # Typically not needed for default VPC
   map_public_ip_on_launch = true
 
   public_subnet_tags = {
