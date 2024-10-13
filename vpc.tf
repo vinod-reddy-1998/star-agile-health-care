85a0d222c12459b4064cadf40f36858fe93c25c2
vinod <100143638+vinod-reddy-1998@users.noreply.github.com>
2024-10-13 21:59:12 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,19 +1,3 @@
-provider "aws" {
-  region = "us-east-1"  # Change to your desired region
-}
-
-data "aws_vpc" "default" {
-  default = true
-}
-
-data "aws_subnet_ids" "default" {
-  vpc_id = data.aws_vpc.default.id
-}
-
-data "aws_subnet" "default_subnets" {
-  count = length(data.aws_subnet_ids.default.ids)
-  id    = data.aws_subnet_ids.default.ids[count.index]
-}
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
