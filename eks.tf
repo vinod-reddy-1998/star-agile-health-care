4a5f042703af711a06b7a9f25d372d1c92285451
vinod <100143638+vinod-reddy-1998@users.noreply.github.com>
2024-10-14 16:02:23 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -8,19 +8,16 @@ module "eks" {
   cluster_addons = {
     coredns = {
       most_recent = true
-      # Set resolve conflicts attributes
       resolve_conflicts_on_create = "OVERWRITE"
       resolve_conflicts_on_update  = "OVERWRITE"
     }
     kube-proxy = {
       most_recent = true
-      # Set resolve conflicts attributes
       resolve_conflicts_on_create = "OVERWRITE"
       resolve_conflicts_on_update  = "OVERWRITE"
     }
     vpc-cni = {
       most_recent = true
-      # Set resolve conflicts attributes
       resolve_conflicts_on_create = "OVERWRITE"
       resolve_conflicts_on_update  = "OVERWRITE"
     }
@@ -30,11 +27,9 @@ module "eks" {
   subnet_ids               = module.vpc.public_subnets
   control_plane_subnet_ids = module.vpc.intra_subnets
 
-  # EKS Managed Node Group(s)
   eks_managed_node_group_defaults = {
     ami_type       = "AL2_x86_64"
     instance_types = ["m5.large"]
-
     attach_cluster_primary_security_group = true
   }
 
@@ -43,10 +38,8 @@ module "eks" {
       min_size     = 1
       max_size     = 2
       desired_size = 2
-
       instance_types = ["t3.large"]
       capacity_type  = "SPOT"
-
       tags = {
         ExtraTag = "helloworld"
       }
