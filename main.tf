abb249119ff154bf3c62914330d5cc734278c8ef
vinod <100143638+vinod-reddy-1998@users.noreply.github.com>
2024-10-14 07:32:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,13 +2,15 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_eks_cluster" "test_cluster" {
-  name     = "test-cluster"
-  role_arn = aws_iam_role.eks_role.arn
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
 
-  vpc_config {
-    subnet_ids = aws_subnet.eks_subnet.*.id
-  }
+resource "aws_subnet" "eks_subnet" {
+  count = 2
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.${count.index}.0/24"
+  availability_zone = element(["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d", "us-east-1f"], count.index)
 }
 
 resource "aws_iam_role" "eks_role" {
@@ -27,12 +29,11 @@ resource "aws_iam_role" "eks_role" {
   })
 }
 
-resource "aws_subnet" "eks_subnet" {
-  count = 2
-  vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.${count.index}.0/24"
-}
+resource "aws_eks_cluster" "test_cluster" {
+  name     = "test-cluster"
+  role_arn = aws_iam_role.eks_role.arn
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  vpc_config {
+    subnet_ids = aws_subnet.eks_subnet[*].id
+  }
 }
