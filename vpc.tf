dfa6e0e76c1061825a602ece91e774a7c70f3fee
vinod <100143638+vinod-reddy-1998@users.noreply.github.com>
2024-10-14 14:11:07 +0530

    Update and rename vpc.t to vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,23 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 4.0"
+
+  name = local.name
+  cidr = local.vpc_cidr
+
+  azs             = local.azs
+  private_subnets = local.private_subnets
+  public_subnets  = local.public_subnets
+  intra_subnets   = local.intra_subnets
+
+  enable_nat_gateway = true
+  map_public_ip_on_launch = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+}
