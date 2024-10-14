786f116a265a93c4cd0557cb5b1721f7d0872dad
vinod <100143638+vinod-reddy-1998@users.noreply.github.com>
2024-10-14 13:50:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -96,7 +96,6 @@ module "eks" {
   eks_managed_node_group_defaults = {
     ami_type       = "AL2_x86_64"
     instance_types = ["m5.large"]
-
     attach_cluster_primary_security_group = true
   }
 
